f1d330f17244623ea3681c91b27215a196d2df39
Student <example@example.com>
2019-01-22 11:56:03 -0800

    Add initial recipe files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe-Box';
+
+  recipes: Recipe[] = [
+    new Recipe('Choco-Chip-Cookie', '1. Chocolate 2. Cookie Dough', 'Mix all ingredients and put in oven');
+    new Recipe('Ice Cream', '1. Chocolate 2. Cookie Dough 3. Ice 4. Cream', 'Mix all ingredients and put in freezer');
+    new Recipe('Chili', '1. Beans 2. Veggies 3. Chili spices', 'Mix all ingredients and put in crockpot');
+  ];
 }
---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,7 @@
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public directions: string){
+    this.title = title,
+    this.ingredients = ingredients,
+    this.directions = directions
+  }
+}
