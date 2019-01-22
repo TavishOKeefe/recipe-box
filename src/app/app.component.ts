80ab6147030365351090f33ca27127f6652a0fcc
Student <example@example.com>
2019-01-22 15:43:16 -0800

    Add something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
   title = 'Recipe-Box';
 
   recipes: Recipe[] = [
-    new Recipe('Choco-Chip-Cookie', '1. Chocolate 2. Cookie Dough', 'Mix all ingredients and put in oven');
-    new Recipe('Ice Cream', '1. Chocolate 2. Cookie Dough 3. Ice 4. Cream', 'Mix all ingredients and put in freezer');
-    new Recipe('Chili', '1. Beans 2. Veggies 3. Chili spices', 'Mix all ingredients and put in crockpot');
+    new Recipe('Choco-Chip-Cookie', ['chocolate', 'cookie dough'], 'Mix all ingredients and put in oven');
+    new Recipe('Ice Cream', ['chocolate', 'ice', 'cream'], 'Mix all ingredients and put in freezer');
+    new Recipe('Chili', ['beans', 'veggies', 'spices'], 'Mix all ingredients and put in crockpot');
   ];
 }
--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,7 +1,12 @@
 export class Recipe {
-  constructor(public title: string, public ingredients: string, public directions: string){
+  constructor(public title: string, public ingredients: array, public directions: string){
     this.title = title,
     this.ingredients = ingredients,
     this.directions = directions
   }
+
+  displayIngredients(){
+    for(let theIngredients of this.ingredients){
+      return theIngredients;
+    }
 }
